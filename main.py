3d164d0822f7316b1dd584becd7abd0481eeb27f
+ <+>
2023-03-28 18:33:52 -0700

    spaceships can move up down left right

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hello Code Blooded!")
 CYAN = (179, 229, 252)
 FPS = 60
+VEL = 5 #velocity
 
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
@@ -27,6 +28,27 @@ def draw_window(red, yellow):
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a]: #LEFT; a key means you can move left at the rate of velocity
+            yellow.x -= VEL
+    if keys_pressed[pygame.K_d]: #RIGHT; a key means you can move right at the rate of velocity
+            yellow.x += VEL
+    if keys_pressed[pygame.K_w]: #UP; note it's the y axis being changed
+            yellow.y -= VEL
+    if keys_pressed[pygame.K_s]: 
+            yellow.y += VEL
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT]: #LEFT; a key means you can move left at the rate of velocity
+            red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT]: #RIGHT; a key means you can move right at the rate of velocity
+            red.x += VEL
+    if keys_pressed[pygame.K_UP]: #UP; note it's the y axis being changed
+            red.y -= VEL
+    if keys_pressed[pygame.K_DOWN]: 
+            red.y += VEL          
+
+
 def main():
     red = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     yellow = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -38,7 +60,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        red.x += 1
+
+        keys_pressed = pygame.key.get_pressed() 
+        yellow_handle_movement(keys_pressed, yellow)
+        
+
         draw_window(red, yellow)
     pygame.quit()
 
